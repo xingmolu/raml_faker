4253705da62431a53fadca9ea35fad7b4abccca9
艾荣彬 <591745264@qq.com>
2015-08-03 23:57:11 +0800

    新增公司，街道等模拟

--- test/test.js
+++ test/test.js
@@ -1,11 +1,27 @@
-var ramlFasker = require('../index');
+var ramlFasker = require('../index'),
+	_ = require("lodash");
 
-console.log(ramlFasker);
 
 var options = {
 	path: 'raml'
 };
 
 ramlFasker.generate(options, function (requestsToMock) {
-	console.log(requestsToMock);
+	//console.log(requestsToMock);
+
+	console.log(requestsToMock.length);
+
+	_.each(requestsToMock, function(reqToMock){
+		console.log('-----------------');
+		console.log('uri ----->' + reqToMock.uri);
+
+		console.log('code ----->' + reqToMock.defaultCode);
+
+		try{
+			console.log(reqToMock.mock());
+		} catch (exception){
+			console.log(exception.stack);
+		}
+		
+	});
 });
